6b5e5125ddfe6d872dc1d902a21c65a61aa772c6
Rafael Sousa <rafael_sousa2018@outlook.com>
2021-09-27 23:06:10 -0300

    pagina de erro criada

--- themes/main/error.php
+++ themes/main/error.php
@@ -1,9 +1,11 @@
-<?php $this->layout('_theme', ['title'=>$title]) ?>
+<?php $this->layout('_theme', ['title' => $title]) ?>
 
 <div class="row">
-    <div class="container">
-        <div class="col-md-12 text-center">
+   <div class="container">
+      <div class="col-md-12 text-center d-flex justify-content-center align-items-center" style="height: 100vh;">
+         <div>
             <h1>Error: <?= $error ?></h1>
-        </div>
-    </div>
+         </div>
+      </div>
+   </div>
 </div>
